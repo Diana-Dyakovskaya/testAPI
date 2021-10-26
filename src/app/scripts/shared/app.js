c74f4cb0e764db85fbd5d265f5cf1343c3b5e758
Diana-Dyakovskaya <diana.dyakovskaya7@gmail.com>
2021-10-26 14:32:22 +0300

    sequential load

--- src/app/scripts/shared/app.js
+++ src/app/scripts/shared/app.js
@@ -1,20 +1,35 @@
 export const app = () => {
   document.addEventListener('DOMContentLoaded', () => {
     // Твой код
-    const btn = document.querySelector('.btn')
+    const btnLoad = document.querySelector('.js-load')
+    const btnLoadMore = document.querySelector('.js-more')
     const app = document.querySelector('#app')
 
-    btn.addEventListener('click', () => {
-      fetch('https://jsonplaceholder.typicode.com/posts')
-        .then((response) => response.json())
-        .then((json) => {
-          console.log(json)
-          json.forEach((element) => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then((response) => response.json())
+      .then((json) => {
+        console.log(json)
+        json.forEach((element) => {
+          if (element.id <= 10) {
             const newElem = document.createElement('article')
             newElem.innerHTML = `<h2 class="card__title">${element.title}</h2>
                                   <p class="card__body">${element.body}</p>`
             app.append(newElem)
-          })
+          }
+        })
+      })
+
+    btnLoadMore.addEventListener('click', () => {
+      fetch('https://jsonplaceholder.typicode.com/posts')
+        .then((response) => response.json())
+        .then((json) => {
+          console.log(json)
+          for (let i = 0; i < 10; i += 1) {
+            const newElem = document.createElement('article')
+            newElem.innerHTML = `<h2 class="card__title">${json[i].title}</h2>
+                                  <p class="card__body">${json[i].body}</p>`
+            app.append(newElem)
+          }
         })
     })
   })
